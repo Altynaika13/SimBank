47c76a501601f481012351348c0a405a71d2371a
Altynai <uhah@MacBook-Pro-Uh.local>
2026-01-15 00:59:00 +0600

    January Promotion
    Tuesday
    
    Monday/Wednesday
    
    Notification
    
    Registration v1
    
    Registration v2
    
    Registratiob v3
    
    Resolved conflict
    
    January Promotion 1
    
    January Promotion 2
    
    Jauary Promotion 3
    
    January Promotion 3
    
    January Promotion 4
    
    January Promotion 5
    
    January Promotion 6

---
+++ src/kg/sim_bank/promotions/JanuaryPromotions.java
@@ -0,0 +1,16 @@
+package kg.sim_bank.promotions;
+
+public class JanuaryPromotions {
+    public static void main(String[] args) {
+
+        System.out.println("January Promotion");
+        System.out.println("January 123");
+        System.out.println("January 1234");
+        System.out.println("January 12345");
+        System.out.println("January 123456");
+        System.out.println("January 1234567");
+        System.out.println("January 12345678");
+        System.out.println("January 123456789");
+
+    }
+}
